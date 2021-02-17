ac466beadac90a2d3a1e66f6979083dd0823e969
Lachlan Stevens <lachlan.stevens@outlook.com>
2021-02-17 17:31:07 +1000

    Update handler.py

--- src/pythonFunction2/handler.py
+++ src/pythonFunction2/handler.py
@@ -4,7 +4,8 @@ def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
     i = [0,3,4,5,32,3,4,5]
-    
+    print("THIS IS MY DEBUGINGGINGINGINGIN")
+    print("made it to this line!!! thanks stackery")
     
     return {
         "testing": random.choice(i)
